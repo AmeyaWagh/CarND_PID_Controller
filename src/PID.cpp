ddf904fe1cd4fda363011f8c6e6cad2c26f9965f
AmeyaWagh <ameya555@gmail.com>
2018-09-08 17:16:10 -0400

    Simple PID implemented to get the car working in the environment

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Ki = Ki;
+    this->Kd = Kd;
+
+    this->p_error=0.0;
+    this->i_error=0.0;
+    this->d_error=0.0;
 }
 
 void PID::UpdateError(double cte) {
+    this->d_error = cte - this->p_error;
+    this->i_error += cte;
+    this->p_error = cte;
 }
 
 double PID::TotalError() {
+    return Kp*p_error+Ki*i_error+Kd*d_error;
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
+  pid.Init(0.15,0.0002,3.8);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +58,24 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+
+          // update error
+          pid.UpdateError(cte);
+          steer_value = -1*pid.TotalError();
+
+          // limit steer_value in bounds
+          if(steer_value > 1.0){
+              steer_value=1.0;
+          }
+          else if(steer_value < -1.0){
+              steer_value=-1.0;
+          }
+
+          ///@todo dynamic throttle
+          // decide throttle values
+
+
+
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
